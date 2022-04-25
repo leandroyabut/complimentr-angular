2cd6cb23aca6b4ed07828dfb6a511d4bf0bef1b9
Leandro Yabut <leandro.yabut@smoothstack.com>
2022-04-24 22:59:54 -0600

    Initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+interface Compliment {
+  compliment: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'complimentr-angular';
+export class AppComponent implements OnInit {
+  
+  compliment = "";
+  loading = true;
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.loadCompliment();
+  }
+
+  loadCompliment() {
+    this.loading = true;
+    this.httpClient.get<Compliment>("https://complimentr.com/api").subscribe(
+      (data) => {
+        this.compliment = data.compliment;
+        this.loading = false;
+      }
+    );
+  }
+  
 }
